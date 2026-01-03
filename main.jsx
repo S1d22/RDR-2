8104207d85d28e3cfb76162d52e79457c8686433
S1d22 <sidalihboudra@gmail.com>
2026-01-03 15:15:20 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', color: '#9d0a0e' }}>
+      <h1>Frontier Redemption Loading...</h1>
+      <p>If you see this, React is working!</p>
+    </div>
+  )
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
